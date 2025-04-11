a834d9fe50d733f9bffbba07518f21bdcfea3f64
Dan Dalpiaz <dpdalpiaz@gmail.com>
2025-04-11 11:41:32 -0500

    add script file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+
 # ga-cookie-script
+
 a small JavaScript file that can clean out accumulated cookies related to Google (Analytics) tags
---
+++ ga-cookie-script.js
@@ -0,0 +1,19 @@
+
+const cookies = document.cookie.split("; ");
+let gaCookies = [];
+
+const scriptTag = document.currentScript;
+const urlParams = new URLSearchParams(scriptTag.src.split("?")[1]);
+const maxCookies = parseInt(urlParams.get("max")) || 30;
+
+cookies.forEach(cookie => {
+    if (cookie.startsWith("_ga_")) {
+        gaCookies.push(cookie.split("=")[0]);
+    }
+});
+
+if (gaCookies.length > maxCookies) {
+    gaCookies.forEach(cookieName => {
+        document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.illinois.edu`;
+    });
+}
